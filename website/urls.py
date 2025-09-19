2d3b7b294ff7de6fff068614d97ddc5a02ccd18f
rumi <pouya.zm83@gmail.com>
2025-09-20 02:00:32 +0330

    added general templates file

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from website.views  import *
 urlpatterns = [
-    path("home",index_views),
+    path("",index_views),
     path("about",about_views),
     path("contact",contact_views)
 ]
--- website/views.py
+++ website/views.py
@@ -5,10 +5,10 @@ from django.http import HttpResponse,JsonResponse
 import requests
 
 def index_views(request):
-    return HttpResponse("<h1>home page</h1>")
+    return render(request, "website/index.html")
 
 def about_views(request):
-    return HttpResponse("<h1>about page</h1>")
+    return render(request, "website/about.html")
  
 def contact_views(request):
-    return HttpResponse("<h1><contact page/h1>")
+    return render(request, "website/contact.html")
