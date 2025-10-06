bb960ed9eaa68af329389f633c84fcf7d67d8196
rumi <pouya.zm83@gmail.com>
2025-10-07 02:14:48 +0330

    costume admin and creat models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    # image =
+    # author = 
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tags = 
+    # category = 
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    title=models.CharField(max_length=225)
+    content=models.TextField()
+class Meta:
+    ordering=['-creating_date']
+
+
+    def __str__(self):
+        return "{} - {}".format(self.title,self.id)
+    
